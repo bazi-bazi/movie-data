394027ac5ae19c09a6e0726aec91db2a92fe18df
bazi <vazha.davituliani@gmail.com>
2020-09-13 18:08:33 +0400

    add basic structure of react carousel

--- src/components/anime/Anime.js
+++ src/components/anime/Anime.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const useStyles = makeStyles({
   gutter: {
@@ -8,15 +11,41 @@ const useStyles = makeStyles({
   },
 });
 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
 const Anime = () => {
   const classes = useStyles();
+
   return (
     <>
       <Grid item container>
         <Grid xs={0} sm={2} />
         <Grid xs={12} sm={8}>
-          <div>
-            <h2 className={classes.gutter}>Movie List </h2>
+          <div className={classes.gutter}>
+            <Carousel responsive={responsive} autoPlay={true} arrows={false}>
+              <Paper>Item 1</Paper>
+              <Paper>Item 1</Paper>
+              <Paper>Item 1</Paper>
+              <Paper>Item 1</Paper>
+            </Carousel>
           </div>
         </Grid>
         <Grid xs={0} sm={2} />
